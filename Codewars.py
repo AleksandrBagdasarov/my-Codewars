e88fb5d8ec850160c5aaa815fee2dd0a53ea1243
Aleksander <sashabagdasarov@gmail.com>
2020-06-26 20:15:18 +0300

    Highest and Lowest

--- Codewars.py
+++ Codewars.py
@@ -1,13 +1,7 @@
-def get_middle(s):
-    if ( not len(s) % 2 ):
-        return(s[len(s) // 2 -1] + s[(len(s) // 2)])
-    else:
-        return(s[len(s[:-1]) // 2])
+def high_and_low(numbers):
+    a = []
+    for x in numbers.split():
+        a.append(int(x))
+    return f'{str(max(a))} {str(min(a))}'
 
-
-
-get_middle("test")#"es")
-get_middle("testing")#"t")
-get_middle("middle")#"dd")
-get_middle("A")#"A")
-get_middle("of")#"of")
+high_and_low("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6")# "542 -214")
