3d2e71377f872e215165e602cc7c23f6d6f38b74
Aleksander <sashabagdasarov@gmail.com>
2020-06-26 18:09:50 +0300

    Mumbling

--- Codewars.py
+++ Codewars.py
@@ -1,34 +1,15 @@
-def increment_string(strng):
-    x = ''
-    if len(strng) == 0 or strng[-1].isalpha():
-        return(strng+'1')
-    else:
-        return(qqq(strng, x))
-
-def qqq(s,x):
-    if len(s) == 0 or s[-1].isalpha():
-        s +='1'
-        s += str(x)
-        return f'{s}'
-    else:
-        q = s[-1]
-        q = int(q)
+def accum(s):
+    q = 1
+    a = []
+    for x in s:
+        x *= q
+        a.append(x.title())
         q += 1
-        if len(str(q)) > 1:
-            x += '0'
-            return qqq(s[0:-1], x)
-        else:
-            s = s[0:-1]
-            s = s+str(q)
-            s += str(x)
-            return f'{s}'
+    print('-'.join(a))
 
-increment_string("foo")# "foo1")
-increment_string("foobar001")# "foobar002" foobar002
-increment_string("foobar1")# "foobar2")
-increment_string("foobar00")# "foobar01")
-increment_string("foobar99")
-increment_string("foobar999")# "foobar100")
-increment_string("foobar099")# "foobar100")
-increment_string("")# "1")
-increment_string("999")
+
+accum("ZpglnRxqenU")# "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")
+accum("NyffsGeyylB")# "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb")
+accum("MjtkuBovqrU")# "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu")
+accum("EvidjUnokmM")# "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm")
+accum("HbideVbxncC")# "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc")
