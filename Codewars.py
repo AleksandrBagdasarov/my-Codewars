67a12387fa52628760e02a3c9409822431fbbce9
Aleksander <sashabagdasarov@gmail.com>
2020-06-26 23:51:45 +0300

    Square Every Digit

--- Codewars.py
+++ Codewars.py
@@ -1,7 +1,7 @@
-def high_and_low(numbers):
-    a = []
-    for x in numbers.split():
-        a.append(int(x))
-    return f'{str(max(a))} {str(min(a))}'
+def square_digits(num):
+    return int(''.join(list(map(str, [int(x)**2 for x in str(num)]))))
 
-high_and_low("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6")# "542 -214")
+
+
+
+square_digits(9119) # 811181
