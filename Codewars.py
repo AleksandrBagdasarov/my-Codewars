8d835373385054c9a3c66d0d3a904a471fd3a9ef
Aleksander <sashabagdasarov@gmail.com>
2020-06-27 11:12:57 +0300

    Jaden Casing Strings

--- Codewars.py
+++ Codewars.py
@@ -1,16 +1,10 @@
-def is_isogram(string):
-    f = ''
-    string = string.lower()
-    for x in string:
-        if string.count(x) != 1:
-            f += x
-    return(len(f)==0)
+def to_jaden_case(string):
+    a = []
+    for x in string.split():
+        a.append(x.capitalize())
+    return(' '.join(a))
 
 
 
-is_isogram("Dermatoglyphics")# True )
-is_isogram("Isogram")# True )
-is_isogram("aba")# False# "same chars may not be adjacent" )
-is_isogram("moOse")# False# "same chars may not be same case" )
-is_isogram("isIsogram")# False )
-is_isogram("")# True# "an empty string is a valid isogram" )
+quote = "How can mirrors be real if our eyes aren't real"
+to_jaden_case(quote) # "How Can Mirrors Be Real If Our Eyes Aren't Real"
