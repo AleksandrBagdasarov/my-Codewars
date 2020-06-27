bd171ae00ca6d40ad10ea22ca414a88ade5826cf
Aleksander <sashabagdasarov@gmail.com>
2020-06-27 10:47:42 +0300

    Isograms

--- Codewars.py
+++ Codewars.py
@@ -1,11 +1,16 @@
-def xo(s):
-    print(s.count('x'))
+def is_isogram(string):
+    f = ''
+    string = string.lower()
+    for x in string:
+        if string.count(x) != 1:
+            f += x
+    return(len(f)==0)
 
 
 
-
-
-xo('')# 'True expected')
-xo('xo')# 'True expected')
-xo('xo0')# 'True expected')
-xo('xxxoo')# 'False expected')
+is_isogram("Dermatoglyphics")# True )
+is_isogram("Isogram")# True )
+is_isogram("aba")# False# "same chars may not be adjacent" )
+is_isogram("moOse")# False# "same chars may not be same case" )
+is_isogram("isIsogram")# False )
+is_isogram("")# True# "an empty string is a valid isogram" )
