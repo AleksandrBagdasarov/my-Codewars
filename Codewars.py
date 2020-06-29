017764219f66ed69e14a5dc7845371edc554b304
Aleksander <sashabagdasarov@gmail.com>
2020-06-29 19:59:08 +0300

    Counting Duplicates

--- Codewars.py
+++ Codewars.py
@@ -1,22 +1,12 @@
-def find(i, seq):
-    s = seq.count(seq[i])
-    if s % 2 == 0:
-        i += 1
-        return find(i, seq)
-    else:
-        return seq[i]
+def duplicate_count(text):
+    duplicate = ''
+    for x in text:
+        if text.lower().count(x) > 1:
+            duplicate += x
+    print(len(set(duplicate)))
 
 
 
-def find_it(seq):
-    i = 0
-    return find(i, seq)
-
-
-
-find_it([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])# 5)
-find_it([1,1,2,-2,5,2,4,4,-1,-2,5])# -1); 
-find_it([20,1,1,2,2,3,3,5,5,4,20,4,5])# 5);
-find_it([10])# 10);
-find_it([1,1,1,1,1,1,10,1,1,1,1])# 10);
-find_it([5,4,3,2,1,5,4,3,2,10,10])# 1);
+duplicate_count("abcdeaB")# 2)
+duplicate_count("abcdea")# 1)
+duplicate_count("indivisibility")# 1)
