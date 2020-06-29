ccea554519dfe3620b63fc986598b5fd2561d64d
Aleksander <sashabagdasarov@gmail.com>
2020-06-29 20:43:06 +0300

    Persistent Bugger.

--- Codewars.py
+++ Codewars.py
@@ -1,12 +1,23 @@
-def duplicate_count(text):
-    duplicate = ''
-    for x in text:
-        if text.lower().count(x) > 1:
-            duplicate += x
-    print(len(set(duplicate)))
+def persistence(n):
+    counter = 0
+    return qqq(n,counter)
+
+def qqq(n,counter):
+    str_n = str(n)
+    str_new_n = str_n[0]
+    int_new_n = int(str_new_n)
+    if n > 9:
+        counter += 1
+        for x in str_n[1:]:
+            int_new_n *= int(x)
+        return qqq(int_new_n, counter)
+    else:
+        return counter
+            
+        
 
 
-
-duplicate_count("abcdeaB")# 2)
-duplicate_count("abcdea")# 1)
-duplicate_count("indivisibility")# 1)
+persistence(39)# 3)
+persistence(4)# 0)
+persistence(25)# 2)
+persistence(999)# 4)
