55d89a4cda589a6963ba2f61c4808fd87040d81c
Aleksander <sashabagdasarov@gmail.com>
2020-06-30 01:00:51 +0300

    Take a Ten Minute Walk

--- Codewars.py
+++ Codewars.py
@@ -1,18 +1,12 @@
-def likes(names):
-    if len(names) == 0:
-        return('no one likes this')
-    elif len(names) == 1:
-        return(f'{names[0]} likes this')
-    elif len(names) == 2:
-        return(f'{names[0]} and {names[1]} like this')
-    elif len(names) == 3:
-        return(f'{names[0]}, {names[1]} and {names[2]} like this')
+def is_valid_walk(walk):
+    if ''.join(walk).count('e') == ''.join(walk).count('w') and ''.join(walk).count('n') == ''.join(walk).count('s') and len(''.join(walk)) == 10:
+        return True
     else:
-        return(f'{names[0]}, {names[1]} and {len(names[2:])} others like this')
+        return False
 
 
-likes([])#, 'no one likes this')
-likes(['Peter'])#, 'Peter likes this')
-likes(['Jacob', 'Alex'])#, 'Jacob and Alex like this')
-likes(['Max', 'John', 'Mark'])#, 'Max, John and Mark like this')
-likes(['Alex', 'Jacob', 'Mark', 'Max'])#, 'Alex, Jacob and 2 others like this')
+
+is_valid_walk(['n','s','n','s','n','s','n','s','n','s'])# 'should return True');
+is_valid_walk(['w','e','w','e','w','e','w','e','w','e','w','e'])# 'should return False');
+is_valid_walk(['w'])# 'should return False');
+is_valid_walk(['n','n','n','s','n','s','n','s','n','s'])# 'should return False');
