6e9c20c96f51d8bc8f5be18ce1d93f7c87ee0171
Aleksander <sashabagdasarov@gmail.com>
2020-07-09 18:05:52 +0300

    задача на подсчет популяции населения

--- Codewars.py
+++ Codewars.py
@@ -1,12 +1,59 @@
-def is_valid_walk(walk):
-    if ''.join(walk).count('e') == ''.join(walk).count('w') and ''.join(walk).count('n') == ''.join(walk).count('s') and len(''.join(walk)) == 10:
-        return True
-    else:
-        return False
+#from math import atan, pi
+#
+#
+#
+#
+#def arcctan(x):
+#    return (pi/2) - atan(x)
+#
+#
+#t = int(input())
+#c = 172
+#T1 =  2000
+#naselenie = (c/45) * arcctan((T1-t)/45)
+#print(naselenie)
+#years_str_list = ['123','200']
+#years_list = [int(x) for x in years_str_list]
+#print(years_list)
+#x = 2020
+#y = naselenie
+#print("%5d - %6.3f миллиард(ов)" % (x, y))
+
+
+from math import atan, pi
 
 
 
-is_valid_walk(['n','s','n','s','n','s','n','s','n','s'])# 'should return True');
-is_valid_walk(['w','e','w','e','w','e','w','e','w','e','w','e'])# 'should return False');
-is_valid_walk(['w'])# 'should return False');
-is_valid_walk(['n','n','n','s','n','s','n','s','n','s'])# 'should return False');
+def arcctan(x):
+    return (pi/2) - atan(x)
+
+def compute_population(t):
+   #вычислить численность населения для года t по формуле
+    c = 172
+    T1 =  2000
+    return (c/45) * arcctan((T1-t)/45)
+
+
+#ввести строку с перечисленными через пробел годами
+line = input()
+
+# преобразовать строку в список из строковых значений годов
+years_str_list = line.split()
+
+# вычислить количество элементов в списке
+n = len(years_str_list)
+
+# сформировать список years_list на основе years_str_list, 
+years_list = [int(x) for x in years_str_list]
+
+
+# создать список population_list, каждый элемент которого вычисляется
+population_list = []
+for x in years_list:
+    population_list.append(compute_population(x))
+
+
+# в цикле для каждого года вывести численность населения, для вывода использовать
+for x, y in zip(years_list, population_list):
+    print("%5d - %6.3f миллиард(ов)" % (x, y))
+# формат "%5d - %6.3f миллиард(ов)"
