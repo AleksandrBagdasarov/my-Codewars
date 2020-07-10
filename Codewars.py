82737c6d0e931fd024512c00ce7a0a9b15e33831
Aleksander <sashabagdasarov@gmail.com>
2020-07-10 11:44:17 +0300

    calculation of the cost of electricity per year

--- Codewars.py
+++ Codewars.py
@@ -1,18 +1,25 @@
+# energy = '150 132 110 186 103 229 179 224 230 113 224 202'
+# norm = 148
+# norm_cost = 3.67
+# over_norm_cost = 4.83
 
+energy = str(input())
+norm = int(input)
+norm_cost = float(input())
+over_norm_cost = float(input())
 
-def compute_payment(s, n, k):
-    # вычисление ежемесячного платежа
-    bank = []
-    for t in range(1, n + 1):
-        x = (s / n) + (s - (t - 1) * (s / n)) * (k / (12 * 100))
-        bank.append(x)
-        print("%2d месяц - %8.2f руб" % (t, x))
-    print("Доход банка - %6.2f руб" % (sum(bank) - s))
-# s=int(input())
-# n=int(input())
-# k=int(input())
-s = 300000
-n = 6
-k = 20
+count_energy = []
+count_cost = 0
 
-compute_payment(s, n, k)
+energy = energy.split()
+for x in energy:
+    x = int(x)
+    count_energy.append(x)
+    if x < norm:
+        count_cost += x * norm_cost
+    else:
+        over = x - norm
+        count_cost += over * over_norm_cost
+        count_cost += norm * norm_cost
+
+print("Сумма: %6d кВт ч, стоимость %7.2f руб" % (sum(count_energy), count_cost))
