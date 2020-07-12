d1c9e6049452289f3f88a711ac7f0702fc73b365
Aleksander <sashabagdasarov@gmail.com>
2020-07-13 01:02:12 +0300

    Bin Search Fun

--- Codewars.py
+++ Codewars.py
@@ -1,28 +1,42 @@
+from sys import exit
 import numpy as np
 
-import numpy.linalg as alg
+def restart():
+    s = str(input())
+    if s.lower() == 'y':
+        input_num()
+    elif s.lower() == 'n':
+        print('Exit Complete!')
+        exit(0)
+
+def bin_search(num, array, count):
+    if num == array[0]:
+        print(f'### {num} Found In {count} Step!')
+        print('### Put "Y" to Restart or "N" to Exit')
+        restart()
+    elif len(array) == 1:
+        print(f'### {num} Not in Array')
+        print('### Put "Y" to Restart or "N" to Exit')
+        restart()
+    elif num > array[len(array) // 2] - 1:
+        count += 1
+        array = array[len(array) // 2 :]
+        bin_search(num, array, count)
+    elif num < array[len(array) // 2] + 1:
+        count += 1
+        array = array[: len(array) // 2]
+        bin_search(num, array, count)
 
 
-a = np.array([[1, -3, -2, 1],[2, 4, 3, -1],[5, -2, 0, -2], [1, 4, 1, -1]])
+def input_num():
+    try:
+        num = int(input('### Put The Number: '))
+        count = 1
+        array = np.sort(np.random.randint(-20, 20, 40))
+        bin_search(num, array, count)
+    except ValueError:
+        print('### This Is Not A Number!')
+        print('### Put "Y" to Restart or "N" to Exit')
+        restart()
 
-print("A :\n", a)
-# Вычислить определитель квадратного блока  матрицы, состоящего из элементов на пересечении первых двух строк  и столбцов с номером 1 и 2, вывести результат:
-
-det_bl = alg.det(a[:2,1:3])
-
-print("det(block) = ", det_bl)
-# Вычислить сумму элементов главной диагонали.матрицы A, вывести результат:
-
-sum_diag = np.sum(np.diag(a))
-
-print("sum_diag =  ", sum_diag)
-# Найти максимальные элементы каждого столбца:
-
-max_col = np.max(a, 0)
-
-print("max_col :", max_col)
-# Найти минимальные элементы каждой строки:
-
-min_row = np.min(a, 1)
-
-print("min_row :", min_row)
+input_num()
